d31aa6b370f72e71de1d961d6d19bf16978e70f9
yeasin692 <mdyeasin94987@gmail.com>
2023-09-12 20:59:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './coponents/Blogs/Blogs'
+import Header from './coponents/Header/Header'
+
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
